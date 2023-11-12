110b01528700309a18b81f57ee3250f020507071
Ali Karimi <96021886+alikarimie96@users.noreply.github.com>
2023-11-12 12:20:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='dsss_homework_2',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        unittest
-    ],
     entry_points={
         'console_scripts': [
             'math-quiz = math_quiz.module_name:main',
